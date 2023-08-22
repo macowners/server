a5b7b5fd71da9166afdd5a921d7f56af89817afd
유진승 <jinseung0327@gmail.com>
2023-08-22 23:07:32 +0900

    API문서페이지

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('/api')
+  const config = new DocumentBuilder() //
+    .setTitle('API')
+    .setDescription('개발을 위한 API 문서입니다.')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build()
+  const document = SwaggerModule.createDocument(app, config) //
+  SwaggerModule.setup('api', app, document) //
   await app.listen(3000)
 }
 bootstrap()
