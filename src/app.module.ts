278cfd2b189afa241f709b1e24aa94e8a1ca1f0b
유진승 <jinseung0327@gmail.com>
2023-08-28 19:39:04 +0900

    morgan update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { SignModule } from './sign/sign.module'
+import { LoggerMiddleware } from './middlewares/logger.middlware'
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/sclife'), UserModule, SignModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
