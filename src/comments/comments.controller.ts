a5eb167be9c6860b497faa71f38d97cc70bf268a
유진승 <jinseung0327@gmail.com>
2023-08-30 16:55:32 +0900

    update

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Get, Post } from '@nestjs/common'
 import { CommentsService } from './comments.service'
 import { Comment } from 'src/schema/comment.schema'
+import { ApiBearerAuth, ApiSecurity, ApiTags } from '@nestjs/swagger'
 
+@ApiBearerAuth()
+@ApiSecurity('basic')
+@ApiTags('Comments')
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
