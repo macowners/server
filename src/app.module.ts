157c123ade6f70c8465175c9867f47249c7a5e94
유진승 <jinseung0327@gmail.com>
2023-08-30 15:45:47 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,11 @@ import { SignModule } from './sign/sign.module'
 import { HealthModule } from './health/health.module'
 import { LoggerMiddleware } from './middlewares/logger.middlware'
 import { JwtAuthGuard } from './guard/AuthGuard'
-import { CommentController } from './comment/comment.controller'
-import { CommentService } from './comment/comment.service'
-import { CommentModule } from './comment/comment.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/sclife'), UserModule, SignModule, HealthModule, CommentModule],
-  controllers: [AppController, CommentController],
-  providers: [AppService, LoggerMiddleware, JwtAuthGuard, CommentService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/sclife'), UserModule, SignModule, HealthModule],
+  controllers: [AppController],
+  providers: [AppService, LoggerMiddleware, JwtAuthGuard],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
